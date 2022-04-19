ecdf8b1e1d8781c09eb4ac38689baf097e31c9d1
Jonathan Secher <secher.jonathan@gmail.com>
2022-04-19 23:11:38 +0200

    Install and configure nelmio/api-doc-bundle used for generate API documentation

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,41 +4,79 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Service\Pagination;
+use OpenApi\Annotations as OA;
 use JMS\Serializer\SerializerInterface;
 use JMS\Serializer\SerializationContext;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class ProductController extends AbstractController
 {
-    private $serializer;
+    private SerializerInterface $serializer;
 
     public function __construct(SerializerInterface $serializer)
     {
         $this->serializer = $serializer;
     }
 
+    /**
+     * @OA\Get(
+     *   tags={"Products"},
+     *   summary="Get all products",
+     *   @OA\Parameter(
+     *     name="page",
+     *     description="Current page number",
+     *     in="query",
+     *     required=false,
+     *     @OA\Schema(type="integer")
+     *   ),
+     *   @OA\Parameter(
+     *     name="limit",
+     *     description="Limit items per page",
+     *     in="query",
+     *     required=false,
+     *     @OA\Schema(type="integer")
+     *   ),
+     *   @OA\Response(response=200, description="All products"),
+     *   @OA\Response(response=401, description="JWT unauthorized error"),
+     *   @OA\Response(response=404, description="No product found")
+     * )
+     */
     #[Route('/api/products', name: 'product_list', methods: ['GET'])]
-    #[Route('/api/products/page/{page}', name: 'product_list_paginated', methods: ['GET'])]
-    #[Route('/api/products/page/{page}/limit/{limit}', name: 'product_list_paginated_limit', methods: ['GET'])]
-    public function list(Pagination $paginator, int $page = 1, int $limit = 10): JsonResponse
+    public function list(Pagination $paginator, Request $request): JsonResponse
     {
-        $result = $paginator->paginate(
-            'SELECT product FROM App\Entity\Product product ORDER BY product.id ASC',
-            $page,
-            $limit,
-            []
-        );
+        $result = $paginator->paginate('SELECT product FROM App\Entity\Product product ORDER BY product.id ASC');
 
         $serializerContext = SerializationContext::create()->setGroups(['product:list']);
-        $json = $this->serializer->serialize($result, 'json', $serializerContext);
-        return new JsonResponse($json, JsonResponse::HTTP_OK, [], true);
+        $jsonProducts = $this->serializer->serialize($result, 'json', $serializerContext);
+
+        $response = new JsonResponse($jsonProducts, JsonResponse::HTTP_OK, [], true);
+        $response->setEtag(md5($response->getContent()));
+        $response->setPublic();
+        $response->isNotModified($request);
+
+        return $response;
     }
 
+    /**
+     * @OA\Get(
+     *   tags={"Products"},
+     *   summary="Get a product by ID",
+     *   @OA\PathParameter(
+     *     name="id",
+     *     description="ID of the product you want to recover"
+     *   ),
+     *   @OA\Response(response=200, description="Product details"),
+     *   @OA\Response(response=401, description="JWT unauthorized error"),
+     *   @OA\Response(response=404, description="No product found with this ID")
+     * )
+     */
     #[Route('/api/products/{id}', name: 'product_show', methods: ['GET'])]
-    public function show(?Product $product): JsonResponse
+    public function show(?Product $product, Request $request): JsonResponse
     {
         if (!$product) {
             throw new NotFoundHttpException("No product found with this ID");
@@ -47,6 +85,11 @@ class ProductController extends AbstractController
         $serializerContext = SerializationContext::create()->setGroups(['product:show']);
         $jsonProduct = $this->serializer->serialize($product, 'json', $serializerContext);
 
-        return new JsonResponse($jsonProduct, jsonResponse::HTTP_OK, [], true);
+        $response = new JsonResponse($jsonProduct, JsonResponse::HTTP_OK, [], true);
+        $response->setEtag(md5($response->getContent()));
+        $response->setPublic();
+        $response->isNotModified($request);
+
+        return $response;
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Service\Pagination;
+use OpenApi\Annotations as OA;
 use JMS\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -28,12 +30,57 @@ class UserController extends AbstractController
         $this->em = $em;
     }
 
+    /**
+     * @OA\Post(
+     *   tags={"Users"},
+     *   summary="Create a new user",
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         type="object",
+     *         @OA\Property(
+     *           property="firstname",
+     *           description="Username for user identification",
+     *           type="string",
+     *           default="John"
+     *         ),
+     *         @OA\Property(
+     *           property="lastname",
+     *           description="User's choosen password",
+     *           type="string",
+     *           default="Doe"
+     *         ),
+     *         @OA\Property(
+     *           property="email",
+     *           description="User's first name",
+     *           type="string",
+     *           default="john.doe@email.com"
+     *         ),
+     *       )
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Created user",
+     *     @OA\JsonContent(
+     *       type="array",
+     *       @OA\Items(ref=@Model(type=User::class, groups={"user:show"}))
+     *     )
+     *   ),
+     *   @OA\Response(response=400, description="JSON field validation failed"),
+     *   @OA\Response(
+     *     response=401,
+     *     description="JWT unauthorized error"
+     *   ),
+     *   @OA\Response(response=500, description="JSON syntax error or no JSON sent in the request body"),
+     * )
+     */
     #[Route('/api/users', name: 'user_create', methods: ['POST'])]
     public function create(Request $request): JsonResponse
     {
-        $jsonReceived = $request->getContent();
-
-        $user = $this->serializer->deserialize($jsonReceived, User::class, 'json');
+        $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
         $user->setCustomer($this->getUser());
         $user->setCreatedAt(new \DatetimeImmutable());
 
@@ -46,40 +93,145 @@ class UserController extends AbstractController
         $this->em->persist($user);
         $this->em->flush();
 
-        $serializerContext = SerializationContext::create()->setGroups(['user:show']);
-        $jsonUser = $this->serializer->serialize($user, 'json', $serializerContext);
+        $jsonUser = $this->serializer->serialize(
+            $user,
+            'json',
+            SerializationContext::create()->setGroups(['user:show'])
+        );
         return new JsonResponse($jsonUser, JsonResponse::HTTP_OK, [], true);
     }
 
+    /**
+     * @OA\Get(
+     *   tags={"Users"},
+     *   summary="Get all users owned by the current customer",
+     *   @OA\Response(response=200, description="All users owned by the current customer"),
+     *   @OA\Response(response=401, description="JWT unauthorized error"),
+     *   @OA\Response(response=404, description="No user found"),
+     *   @OA\Parameter(
+     *     name="page",
+     *     description="Current page number",
+     *     in="query",
+     *     required=false,
+     *     @OA\Schema(type="integer")
+     *   ),
+     *   @OA\Parameter(
+     *     name="limit",
+     *     description="Limit items per page",
+     *     in="query",
+     *     required=false,
+     *     @OA\Schema(type="integer")
+     *   )
+     * )
+     */
     #[Route('/api/users', name: 'user_list', methods: ['GET'])]
-    #[Route('/api/users/page/{page}', name: 'user_list_paginated', methods: ['GET'])]
-    #[Route('/api/users/page/{page}/limit/{limit}', name: 'user_list_paginated_limit', methods: ['GET'])]
-    public function list(Pagination $paginator, int $page = 1, int $limit = 10): JsonResponse
+    public function list(Pagination $paginator, Request $request): JsonResponse
     {
         $result = $paginator->paginate(
-            'SELECT user FROM App\Entity\User user WHERE user.customer = :id ORDER BY user.id DESC',
-            $page,
-            $limit,
+            'SELECT user
+            FROM App\Entity\User user
+            WHERE user.customer = :id
+            ORDER BY user.id DESC',
             ['id' => $this->getUser()->getId()]
         );
 
         $serializerContext = SerializationContext::create()->setGroups(['user:list']);
         $jsonUsers = $this->serializer->serialize($result, 'json', $serializerContext);
 
-        return new JsonResponse($jsonUsers, JsonResponse::HTTP_OK, [], true);
+        $response = new JsonResponse($jsonUsers, JsonResponse::HTTP_OK, [], true);
+        $response->setEtag(md5($response->getContent()));
+        $response->setPublic();
+        $response->isNotModified($request);
+
+        return $response;
     }
 
+    /**
+     * @OA\Get(
+     *   tags={"Users"},
+     *   summary="Get a user by ID",
+     *   @OA\Response(response=200, description="User details"),
+     *   @OA\Response(response=401, description="JWT unauthorized error"),
+     *   @OA\Response(response=404, description="No user found with this ID"),
+     *   @OA\PathParameter(
+     *     name="id",
+     *     description="ID of the user you want to recover"
+     *   )
+     * )
+     */
     #[Route('/api/users/{id}', name: 'user_show', methods: ['GET'])]
-    public function show(?User $user): JsonResponse
+    public function show(?User $user, Request $request): JsonResponse
     {
         $this->userNotExist($user);
-        $this->isNotOwner('USER_SHOW', $user, 'You are not authorized to see this content');
+        $this->isNotOwner(
+            'USER_SHOW',
+            $user,
+            'You are not authorized to see this content'
+        );
 
         $serializerContext = SerializationContext::create()->setGroups(['user:show']);
-        $jsonUser = $this->serializer->serialize($user, 'json', $serializerContext);
-        return new JsonResponse($jsonUser, JsonResponse::HTTP_OK, [], true);
+        $jsonUser = $this->serializer->serialize(
+            $user,
+            'json',
+            $serializerContext
+        );
+
+        $response = new JsonResponse($jsonUser, JsonResponse::HTTP_OK, [], true);
+        $response->setEtag(md5($response->getContent()));
+        $response->setPublic();
+        $response->isNotModified($request);
+
+        return $response;
     }
 
+    /**
+     * @OA\Put(
+     *   tags={"Users"},
+     *   summary="Update a user by ID",
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         type="object",
+     *         @OA\Property(
+     *           property="firstname",
+     *           description="Username for user identification",
+     *           type="string",
+     *           default="John"
+     *         ),
+     *         @OA\Property(
+     *           property="lastname",
+     *           description="User's choosen password",
+     *           type="string",
+     *           default="Doe"
+     *         ),
+     *         @OA\Property(
+     *           property="email",
+     *           description="User's first name",
+     *           type="string",
+     *           default="john.doe@email.com"
+     *         ),
+     *       )
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="User successfully updated",
+     *     @OA\JsonContent(
+     *       type="array",
+     *       @OA\Items(ref=@Model(type=User::class, groups={"user:show"}))
+     *     )
+     *   ),
+     *   @OA\Response(response=400, description="JSON field validation failed"),
+     *   @OA\Response(
+     *         response=401,
+     *         description="JWT unauthorized error"
+     *   ),
+     *   @OA\Response(response=404, description="No user found with this ID"),
+     *   @OA\Response(response=500, description="JSON syntax error or no JSON sent in the request body"),
+     * )
+     */
     #[Route('/api/users/{id}', name: 'user_update', methods: ['PUT'])]
     public function update(Request $request, ?User $user): JsonResponse
     {
@@ -106,6 +258,19 @@ class UserController extends AbstractController
         return new JsonResponse($jsonUser, JsonResponse::HTTP_OK, [], true);
     }
 
+    /**
+     * @OA\Delete(
+     *   tags={"Users"},
+     *   summary="Delete a user by ID",
+     *   @OA\Response(response=200, description="Success message"),
+     *   @OA\Response(response=401, description="JWT unauthorized error"),
+     *   @OA\Response(response=404, description="No user found with this ID"),
+     *   @OA\PathParameter(
+     *     name="id",
+     *     description="ID of the user you want to delete"
+     *   )
+     * )
+     */
     #[Route('/api/users/{id}', name: 'user_delete', methods: ['DELETE'])]
     public function delete(?User $user): JsonResponse
     {
